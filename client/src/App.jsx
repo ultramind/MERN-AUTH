8a1c3310bccc9c02d032de3558aa14c24c803d3c
ultragrace <udechukwuakachukwu.com>
2023-03-18 02:35:01 +0100

    created and styled the username component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Importing th pages/componets
+
+import Username from "./components/Username";
+import Profile from "./components/Profile";
+import Password from "./components/Password";
+import Reset from "./components/Reset";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Root routes
+  const routers = createBrowserRouter([
+    {
+      path: "/",
+      element: <Username></Username>,
+    },
+    {
+      path: "/register",
+      element: <Register></Register>,
+    },
+    {
+      path: "/password",
+      element: <Password></Password>,
+    },
+    {
+      path: "/profile",
+      element: <Profile></Profile>,
+    },
+    {
+      path: "/recovery",
+      element: <Recovery></Recovery>,
+    },
+    {
+      path: "/reset",
+      element: <Reset></Reset>,
+    },
+    {
+      path: "*",
+      element: <PageNotFound></PageNotFound>,
+    },
+  ]);
 
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  )
+    <main>
+      <RouterProvider router={routers}></RouterProvider>
+    </main>
+  );
 }
 
-export default App
+export default App;
